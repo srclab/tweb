7336f09599208dc79fc2207c71cabb2322db6f32
luxcgo <luxcgo@gmail.com>
2023-04-26 01:13:40 +0800

    v1 -> v2

---
+++ v2/server_test.go
@@ -0,0 +1,16 @@
+package v2
+
+import (
+	"net/http"
+	"testing"
+)
+
+func TestServer(t *testing.T) {
+	s := NewHTTPServer()
+
+	s.addRoute(http.MethodGet, "/user", func(ctx *Context) {
+		ctx.Resp.Write([]byte("hello, user"))
+	})
+
+	s.Start(":8081")
+}
